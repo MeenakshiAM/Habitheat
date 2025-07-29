fb119f5a88624012e948de8a65eb75d3cac34d14
Shadow <das840kunal@gmail.com>
2025-07-29 09:35:03 +0530

    bug83/fixed

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,9 @@
 import express from "express";
-import cors from "cors";
 import authRoutes from "./routes/auth.js";
 import errorHandler from "./middleware/errorHandler.js";
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 
 // API routes
